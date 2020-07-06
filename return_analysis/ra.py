ae773ecf69c1bf9d5769462a927a03de4283be36
Niels Heijmans <niels_heijmans@icloud.com>
2020-07-06 20:21:38 +0200

    Added return analysis Lambda, so the data of an sample based on md5 can be queried

--- return_analysis/ra.py
+++ return_analysis/ra.py
@@ -1,13 +1,18 @@
 import json
 import boto3
-from os import environ
+from os import environ as env
 
+dynclient = boto3.client('dynamodb')
 def lambda_handler(event, context):
-    print(event)
 
+    resp = dynclient.get_item(
+            TableName=env['Table'],
+            Key={
+                'md5': {'S': event['pathParameters']['proxy']}
+                }
+        )
+    
     return {
         "statusCode": 200,
-        "body": json.dumps(
-            {"message": "success"}
-        ),
+        "body": json.dumps(resp['Item']),
     }
